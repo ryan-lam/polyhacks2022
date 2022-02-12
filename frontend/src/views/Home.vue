23f70d3ef1ab0030f99efb949d194d3acfffeea5
Jaxz10 <42484410+Jaxz102@users.noreply.github.com>
2022-02-12 02:05:38 -0500

    Finished Homepage

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,23 +1,35 @@
 <template>
-  <nav>
-    <h1>COURODE</h1>
-	<section class="links">
-		<div class="links__item">
-			<a href="#">Home</a>
-		</div>
-		<div class="links__item">
-			<a href="#">Features</a>
-		</div>
-		<div class="links__item">
-			<a href="#">Resources</a>
-		</div>
-		<div class="links__item">
-			<a href="#">Contact</a>
-		</div>
+	<main>
+		<nav>
+			<h1>COURODE</h1>
+			<section class="links">
+				<div class="links__item">
+					<a href="#">Home</a>
+				</div>
+				<div class="links__item">
+					<a href="#">Features</a>
+				</div>
+				<div class="links__item">
+					<a href="#">Resources</a>
+				</div>
+				<div class="links__item">
+					<a href="#">Contact</a>
+				</div>
 
-	</section>
-    
-  </nav>
+			</section>
+		</nav>
+		
+		<section class="content">
+			<h1 class="content__title">Revolutionizing the Education System with AI.</h1>
+			<p class="content__subtitle">Transfer speech to text including a range of other useful tools</p>
+			<div class="content__buttons">
+				<button class="content__buttons--teacher">Teacher Login</button>
+				<button class="content__buttons--student">Student Login</button>
+			</div>
+		</section>
+		<img src="../assets/img/bg15-2.png" alt="banner" class="imgone">
+	</main>
+
 </template>
 
 <script>
@@ -35,20 +47,20 @@ export default {
 @import "../assets/styles.scss";
 
 nav{
-	
+	position: relative;
 	width: 100%;
 	height: 80px;
-	background-color: red;
-	position: relative;
+
+
 
 	& > h1{
 	
 		position: absolute;
 		transform: translate(0, -50%);
 		top: 50%;
-		left: 40px;
+		left: 80px;
 		vertical-align: middle;
-		background-color: blue;
+
 		font-size: 2rem;
 	}
 
@@ -63,19 +75,82 @@ nav{
 	align-items: stretch;
 	justify-content: stretch;
 	height: 100%;
-
+	margin-right: 80px;
 	&__item{
-		width: 100px;
-		background-color: aqua;
-
+		width: 120px;
 		& > a{
-			padding-top: 20px;
+			margin-top: 25px;
+			display: block;
+		
 			text-decoration: none;
+			font-size: 20px;
+
+			&:hover{
+				color: $secondary-colour-hover;
+			}
 
 		}
-		
-
 	}
+}
+
+main{
+	position: relative;
+	height: 100vh;
+	background-color: $bgcolour;
+
+}
+
+.content{
 	
+	width: 50%;
+	position: absolute;
+	top: 50%;
+	z-index: 2;
+	transform: translate(80px, -50%);
+	&__title{
+		font-size: 50px;
+		text-align: left;
+	}
+	&__subtitle{
+		font-size: 25px;
+		text-align: left;
+		
+	}
+	&__buttons{
+		margin-top: 40px;
+
+		height: 50px;
+		display: flex;
+		flex-direction: row;
+		width: 100%;
+		justify-content: flex-start;
+		gap: 40px;
+		align-items: stretch;
+
+		&--teacher{
+			background-color: $primary-colour;
+			&:hover{
+				background-color: $primary-colour-hover;
+			}
+		}
+		&--student{
+			background-color: $secondary-colour;
+			&:hover{
+				background-color: $secondary-colour-hover;
+			}
+		}
+		
+	}
+
+}
+.imgone{
+	width: 500px;
+	display: block;
+	position: absolute;
+	top: 50%;
+	right: 0;
+	transform: translate(-80px, -55%);
+	z-index: 1;
+
 }
 </style>
