54746e8018dffc97075fc696b66806ed500972e0
ryan-lam <ryan-lam@users.noreply.github.com>
2022-02-13 06:16:22 -0500

    done content config

--- backend/routes/uploadcontent.js
+++ backend/routes/uploadcontent.js
@@ -52,9 +52,9 @@ router.post('/convert', upload.single('file'), async (req, res) => {
     if(req.file){
         // add file to server
         console.log("add file to server")
-        // console.log("C:/Users/Ryan Lam/Desktop/polyhacks2022/backend/" + req.file.path)
-        // const filePath = "C:/Users/Ryan Lam/Desktop/polyhacks2022/backend/" + req.file.path
-        const filePath = "/Users/jaxz/Desktop/polyhacks2022/backend/" + req.file.path
+        console.log("C:/Users/Ryan Lam/Desktop/polyhacks2022/backend/" + req.file.path)
+        const filePath = "C:/Users/Ryan Lam/Desktop/polyhacks2022/backend/" + req.file.path
+        // const filePath = "/Users/jaxz/Desktop/polyhacks2022/backend/" + req.file.path
         const fileName = req.file.path
         // upload file to cloud
         console.log("Add file to gcloud")
@@ -97,8 +97,7 @@ router.post('/convert', upload.single('file'), async (req, res) => {
     console.log(CONTENT_DATA)
     // RETURN THE CONTENT
     // return res.send("ok")
-    // return res.json({fileURL: fileURL, transcribe: transcription})
-    return res.send(CONTENT_DATA)
+    return res.json({data: CONTENT_DATA})
 })
 
 const getTranscibe = async (fileURL) => {
